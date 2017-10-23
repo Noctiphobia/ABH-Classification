68aae1a903fdb55373ceb17a0146da6e66bcdbf5
Ahmed Abdelkarim <abdelkarima@student.mini.pw.edu.pl>
2017-10-23 12:40:37 +0200

    correlation

--- preprocessing.R
+++ preprocessing.R
@@ -12,8 +12,27 @@ cols = apply(ABHnoID,2,function(x){length(unique(x))})
 ABHnoID = ABHnoID[,names(cols[cols>1])]
 #change chars to factors
 for (i in 1:length(ABHnoID)) {
-  if (is.character(ABHnoID[,i]))
+  if (is.character(ABHnoID[,i])) {
     ABHnoID[,i] = as.factor(ABHnoID[,i])
+    ABHnoID[,i] = addNA(ABHnoID[,i])
+  }
+}
+library(caret)
+cor2 = cor(apply(ABHnoID,2, as.numeric), use = "pairwise.complete.obs")
+
+corx = character(0)
+cory = character(0)
+correlation = numeric(0)
+for (i in 1:length(ABHnoID)) {
+  for (j in 1:length(ABHnoID)) {
+    if (i != j) {
+      corx[length(corx) + 1] = rownames(cor2)[i]
+      cory[length(cory) + 1] = colnames(cor2)[j]
+      correlation[length(correlation) + 1] = cor2[i,j]
+    }
+  }
 }
 
-
+corres = na.omit(data.frame(corx, cory, correlation))
+corresfiltered = corres[corres$correlation > 0.5,]
+corresfiltered = corresfiltered[order(corresfiltered$correlation, decreasing = T),]
