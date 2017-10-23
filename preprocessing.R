4d6b9330aa87973dcb1873a685b65ec2d0fc0963
Jakub Bondyra <jb10193@gmail.com>
2017-10-23 11:37:40 +0200

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -4,3 +4,7 @@ names(ABHnoID)[134] = "MpcPaid_c"
 names(ABHnoID)[127] = "y"
 ABHnoID = ABHnoID[,!grepl(".*modified.*", names(ABHnoID), ignore.case = T)]
 ABHnoID = ABHnoID[,!grepl(".*token*", names(ABHnoID), ignore.case = T)]
+cols = apply(ABHnoID,2,function(x){length(unique(x))})
+
+#usuwanie atrybutow o jednej lub mniej wartosciach
+colnames2 = names(cols[cols>1])
