f38287eb2b120d8d4bf45a7250eba35fefa0b973
Ahmed Abdelkarim <abdelkarima@student.mini.pw.edu.pl>
2017-10-23 11:26:16 +0200

    Preprocessing - removed 'modified' and 'token' columns

--- preprocessing.R
+++ preprocessing.R
@@ -2,3 +2,5 @@ load('ABH.rda')
 ABHnoID = cbind(ABH[,!grepl(".*id.*", names(ABH), ignore.case = T)], ABH$MpcPaid__c)
 names(ABHnoID)[134] = "MpcPaid_c"
 names(ABHnoID)[127] = "y"
+ABHnoID = ABHnoID[,!grepl(".*modified.*", names(ABHnoID), ignore.case = T)]
+ABHnoID = ABHnoID[,!grepl(".*token*", names(ABHnoID), ignore.case = T)]
